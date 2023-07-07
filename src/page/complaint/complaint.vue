8218611de9d73931e30fe90e73e0ae05ff47b64b
Ericgigit <117895649+Ericgigit@users.noreply.github.com>
2023-07-07 17:01:54 +0800

    实现图片上传和图片显示功能

--- src/page/complaint/complaint.vue
+++ src/page/complaint/complaint.vue
@@ -53,6 +53,10 @@
 				</el-descriptions-item>
 				<el-descriptions-item label="投诉内容">{{item.submitContext}}</el-descriptions-item>
 			</el-descriptions>
+			
+			<!-- 图片显示区域 -->
+			<img v-if="item.submitIma" style="margin: 10px;" width="auto" height="60px" :src="'data:image/png;base64,'+item.submitIma"/>
+			
 			<el-steps :space="200" :active="item.state" finish-status="success" style="margin-top: 15px;">
 				<el-step title="已提交"></el-step>
 				<el-step title="分配处理人员"></el-step>
@@ -75,40 +79,22 @@
 
 		<!-- 投诉表单 -->
 		<el-dialog class="register-panel" :title="title" :visible.sync="open" width="500px" append-to-body>
-			<el-form :model="suit_form" :rules="rules" ref="suit_form" label-width="100px" class="demo-suit-form">
+			
+			<el-form ref="importFormRef" :rules="rules" :model="importForm" label-width="130px">
 				<el-form-item label="投诉内容" prop="submitcontext">
 					<el-input type="textarea" v-model="suit_form.submitContext"></el-input>
 				</el-form-item>
-
-				<!-- <el-upload action="#" list-type="picture-card" :auto-upload="false" :limit="limitNum">
-					<i slot="default" class="el-icon-plus"></i>
-					<div slot="file" slot-scope="{file}">
-						<img class="el-upload-list__item-thumbnail" :src="file.url" alt="">
-						<span class="el-upload-list__item-actions">
-							<span class="el-upload-list__item-preview" @click="handlePictureCardPreview(file)">
-								<i class="el-icon-zoom-in"></i>
-							</span>
-							<span v-if="!disabled" class="el-upload-list__item-delete" @click="handleRemove(file)">
-								<i class="el-icon-delete"></i>
-							</span>
-						</span>
-					</div>
-				</el-upload>
-				<el-dialog :visible.sync="dialogVisible" append-to-body>
-					<img width="100%" :src="dialogImageUrl" alt="">
-				</el-dialog> -->
-
-				<el-upload ref="carUpload" action="http://192.168.27.30:8080/suit/upload" :file-list="carPictureList" list-type="picture-card"
-					:limit="limitNum" :auto-upload="true" :on-change="changeCarPicture" :on-exceed="handleExceed"
-					:before-upload="beforeUpload" style="margin-bottom: 20px;">
-					<i class="el-icon-plus"></i>
-					
-				</el-upload>
-
-
+				
+				<el-form-item label="上传文件:" prop="excel">
+					<el-upload class="upload-demo" ref="upload" action="http://192.168.27.30:8080/suit/upload"
+						:http-request="httpRequest" :before-upload="beforeUpload" :on-exceed="handleExceed" 
+						:on-change="changeCarPicture" :limit="1">
+						<el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+						<div slot="tip" class="el-upload__tip">只能上传.xlsx文件，且不超过5M</div>
+					</el-upload>
+				</el-form-item>
 				<el-form-item>
-					<el-button type="primary" @click="submitForm()">立即提交</el-button>
-					<el-button @click="resetForm()">重置</el-button>
+					<el-button type="primary" @click="submitImportForm">立即提交</el-button>
 				</el-form-item>
 			</el-form>
 		</el-dialog>
@@ -145,6 +131,19 @@
 	export default {
 		data() {
 			return {
+				//导入表单数据
+				importForm: {
+					kgCode: '',
+					targetUrl: '',
+					suitId: '',
+					submitId: '',
+				},
+				//存放上传文件
+				fileList: [],
+				carPicture: null,
+				
+				
+				imgDecoder: "data:image/png;base64,",
 				dialogImageUrl: '',
 				dialogVisible: false,
 				disabled: false,
@@ -227,7 +226,6 @@
 
 				// doUpload: this.$http.adornUrl('/sys/dept/upload'),
 				carPictureList: [],
-				carPicture: '' //定义一个值来接收base64的字符串 
 			};
 		},
 		methods: {
@@ -239,33 +237,91 @@
 					let _base64 = reader.result;
 					let BASE64 = _base64.split(",");
 					this.carPicture = BASE64[1]; //赋值
+					console.log(this.carPicture);
 				};
-				// reader.readAsDataURL(file.raw);
+				reader.readAsDataURL(file.raw);
 			},
-			handleExceed(files) {
-				//这个是文件超出个数限制时，给出提示
-				this.$notify.warning({
-					title: '警告',
-					message: '图片只能上传一张'
+			// 覆盖默认的上传行为，可以自定义上传的实现，将上传的文件依次添加到fileList数组中,支持多个文件
+			httpRequest(option) {
+				this.fileList.push(option)
+			},
+			// 上传前处理
+			beforeUpload(file) {
+				let fileSize = file.size
+				const FIVE_M = 5 * 1024 * 1024;
+				//大于5M，不允许上传
+				if (fileSize > FIVE_M) {
+					this.$message.error("最大上传5MB")
+					return false
+				}
+				//判断文件类型，必须是xlsx格式
+				// let types = ['image/jpeg', 'image/jpg', 'image/png', 'video/mp4'];
+				// const isImage = types.includes(file.type);
+				// if (!isImage) {
+				// 	this.$message.error('上传图片只能是 JPG、JPEG、PNG 格式!');
+				// 	return false;
+				// }
+				// let reg = /^.+(\.xlsx)$/
+				// if (!reg.test(fileName)) {
+				// 	this.$message.error("只能上传xlsx!")
+				// 	return false
+				// }
+				return true
+			},
+			// 文件数量过多时提醒
+			handleExceed() {
+				this.$message({
+					type: 'error',
+					message: '最多支持1个附件上传'
 				})
 			},
-			beforeUpload(file) {
-				//添加文件类型的限制
-				let types = ['image/jpeg', 'image/jpg', 'image/png'];
-				const isImage = types.includes(file.type);
-				if (!isImage) {
-					this.$message.error('上传图片只能是 JPG、JPEG、PNG 格式!');
-					return false;
+			//导入Excel病种信息数据
+			submitImportForm(file) {
+				console.log(file);
+				// 使用form表单的数据格式
+				const params = {
+					submitId: this.user.personId,
+					submitIma: this.carPicture,
+					submitContext: this.suit_form.submitContext,
 				}
+				console.log(this.fileList);
+				// 将上传文件数组依次添加到参数paramsData中
+				// this.fileList.forEach((x) => {
+				// 	paramsData.append('file', x.file)
+				// });
+				// // 将输入表单数据添加到params表单中
+				// params.append('kgCode', this.importForm.kgCode)
+				// params.append('targetUrl', this.importForm.targetUrl)
+				// params.append('suitId', this.importForm.suitId)
+				// params.append('submitId', this.importForm.submitId)
+			
+				submitSuitForm(params).then(res => {
+					console.log(res);
+					this.$message.success('新增成功');
+					this.open = false;
+					this.getList();
+				});
+				//这里根据自己封装的axios来进行调用后端接口
+				// this.httpPostWithLoading(paramsData).then(match => {
+				// 	if (match.success) {
+				// 		this.$message({
+				// 			message: "导入成功",
+				// 			type: "success"
+				// 		})
+				// 	} else {
+				// 		this.$message({
+				// 			message: "导入失败",
+				// 			type: "error"
+				// 		})
+				// 	}
+				// 	this.$refs.importFormRef.resetFields() //清除表单信息
+				// 	this.$refs.upload.clearFiles() //清空上传列表
+				// 	this.fileList = [] //集合清空
+				// 	this.dialogVisible1 = false //关闭对话框
+			
+				// })
 			},
 
-			handleRemove(file) {
-				console.log(file);
-			},
-			handlePictureCardPreview(file) {
-				this.dialogImageUrl = file.url;
-				this.dialogVisible = true;
-			},
 			creatComplaint() {
 				this.open = true;
 				this.title = "新建投诉";
@@ -274,7 +330,6 @@
 			submitForm() {
 				this.$refs["suit_form"].validate(valid => {
 					if (valid) {
-						this.suit_form.state = 1;
 						console.log(this.suit_form);
 						submitSuitForm(this.suit_form).then(res => {
 							console.log(res);
